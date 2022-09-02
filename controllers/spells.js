40145d78f88241e963232c463a8d63a4a58bfd6f
Glitch (tidal-relieved-parent) <none>
2022-09-02 01:17:09 +0000

    🚡😚 Checkpoint ./views/spells.ejs:41301976/473:48254170/11 ./controllers/spells.js:41301976/5059

--- controllers/spells.js
+++ controllers/spells.js
@@ -1,56 +1,67 @@
-const Spell = require('../models/Spell')
+const Spell = require("../models/Spell");
 
 module.exports = {
-    getSpells: async (req,res)=>{
-        console.log(req.user)
-        try{
-            const spells = await Spell.find({userId:req.user.id})
-            res.render('spells.ejs', {spells: spells, user: req.user})
-        }catch(err){
-            console.log(err)
-        }
-    },
-    createSpell: async (req, res)=>{
-        try{
-            await Spell.create({name: req.body.name, description: req.body.description, level: req.body.level, userId: req.user.id})
-            console.log('Spell has been added!')
-            res.redirect('/spells')
-        }catch(err){
-            console.log(err)
-        }
-    },
-  
-  
-    // markComplete: async (req, res)=>{
-    //     try{
-    //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
-    //             completed: true
-    //         })
-    //         console.log('Marked Complete')
-    //         res.json('Marked Complete')
-    //     }catch(err){
-    //         console.log(err)
-    //     }
-    // },
-    // markIncomplete: async (req, res)=>{
-    //     try{
-    //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
-    //             completed: false
-    //         })
-    //         console.log('Marked Incomplete')
-    //         res.json('Marked Incomplete')
-    //     }catch(err){
-    //         console.log(err)
-    //     }
-    // },
-    // deleteTodo: async (req, res)=>{
-    //     console.log(req.body.todoIdFromJSFile)
-    //     try{
-    //         await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
-    //         console.log('Deleted Todo')
-    //         res.json('Deleted It')
-    //     }catch(err){
-    //         console.log(err)
-    //     }
-    // }
-}    
+  getSpells: async (req, res) => {
+    console.log(req.user);
+    try {
+      const spells = await Spell.find({ userId: req.user.id });
+      res.render("spells.ejs", { spells: spells, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createSpell: async (req, res) => {
+    try {
+      //find the spell in the api
+      const response = await fetch(`https://www.dnd5eapi.co/api/spells/${req.body.name}`);
+      
+      //found
+      const spell = await response.json();
+      
+      //not found
+      await Spell.create({
+        name: req.body.name,
+        description: req.body.description,
+        level: req.body.level,
+        userId: req.user.id,
+      });
+      console.log("Spell has been added!");
+      res.redirect("/spells");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  // markComplete: async (req, res)=>{
+  //     try{
+  //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+  //             completed: true
+  //         })
+  //         console.log('Marked Complete')
+  //         res.json('Marked Complete')
+  //     }catch(err){
+  //         console.log(err)
+  //     }
+  // },
+  // markIncomplete: async (req, res)=>{
+  //     try{
+  //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+  //             completed: false
+  //         })
+  //         console.log('Marked Incomplete')
+  //         res.json('Marked Incomplete')
+  //     }catch(err){
+  //         console.log(err)
+  //     }
+  // },
+  // deleteTodo: async (req, res)=>{
+  //     console.log(req.body.todoIdFromJSFile)
+  //     try{
+  //         await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
+  //         console.log('Deleted Todo')
+  //         res.json('Deleted It')
+  //     }catch(err){
+  //         console.log(err)
+  //     }
+  // }
+};
